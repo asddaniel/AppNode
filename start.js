fc4df7a6a244d9369cde107f589e1f80e37409c8
Daniel Assani <devasddaniel@gmail.com>
2022-12-25 19:57:26 +0200

    watcher

---
+++ start.js
@@ -0,0 +1,31 @@
+import { spawn } from "child_process"
+import { watch } from "fs/promises"
+
+const [node, _, file] = process.argv
+
+function processus(){
+    const process = spawn(node, [file])
+
+    process.stdout.on("data", (data)=>{
+        console.log(data.toString("utf8"))
+    })
+    
+    process.stderr.on("data", (data)=>{
+        console.error(data.toString("utf8"))
+    })
+    process.on("close", (code)=>{
+        console.log("process exited with "+code);
+    })
+
+   return process;
+}
+
+const processus_enfant = processus()
+const watcher  = watch("./", {recursive:true})
+for await (const event of watcher){
+        if(event.filename.endsWith(".js")){
+            processus_enfant.kill()
+            processus_enfant = processus()
+        }
+}
+
