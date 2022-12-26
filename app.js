7e16f99b677840d26a4cbc58346440ed5a17ee21
Daniel Assani <devasddaniel@gmail.com>
2022-12-26 07:16:16 +0200

    image add

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 import {createServer} from 'node:http'
 import { text } from 'node:stream/consumers';
+import { buffer } from 'stream/consumers';
 import route from './route.js';
 
 const server =  createServer(async (req, res)=>{
     
-    // const url = new URL(req.url, `http://${req.headers.host}`)
+     const url = new URL(req.url, `http://${req.headers.host}`)
     //method  getparameters
-    console.log(req.url)
+     console.log(req.buffer)
     if(req.method=="POST"){
         let get_response = async function(){
-            return await text(req)
+            return await buffer(req)
         }
-        get_response().then((e)=>console.log(e))
+        get_response().then((e)=>{
+            res.end(e);
+        })
     }else{
        const routes =  await route(req.url)
     
